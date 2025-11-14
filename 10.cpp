8125dde3c50d48b87b56b35ba59294f0dfe101eb
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2025-11-15 00:32:10 +0530

    first commit

--- 10.cpp
+++ 10.cpp
@@ -4,89 +4,89 @@
 #include <iostream>
 using namespace std;
 
-class Game{
+// ---- Base Class (Polymorphism Source) ----
+class Game {
 public:
-    virtual void play() = 0;
+    virtual void play() = 0;   // pure virtual (abstract)
 };
 
-class Tic: public Game{
+// ---- Derived Class Implementing Tic Tac Toe ----
+class TicTacToe : public Game {
     char board[3][3];
     char player;
 
 public:
-    Tic(){
+    TicTacToe() {
         player = 'X';
-        for(int i = 0 ; i < 3 ; i++){
-            for(int j = 0 ; j < 3 ; j++){
-                board[i][j]= ' ';
-            }
-        }
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                board[i][j] = ' ';
     }
 
-    void display(){
-        cout<<endl;
-        for(int i = 0 ; i < 3 ; i++){
-            cout<<" ";
-            for(int j = 0 ; j < 3 ; j++){
-                cout<<board[i][j];
-                if(j<2) cout<<" | ";
+    void display() {
+        cout << "\n";
+        for(int i=0;i<3;i++) {
+            cout << " ";
+            for(int j=0;j<3;j++) {
+                cout << board[i][j];
+                if(j < 2) cout << " | ";
             }
-            if(i<2) cout<<"\n----------------\n";
+            if(i < 2) cout << "\n-----------\n";
         }
-        cout<<endl;
+        cout << "\n";
     }
 
-    bool win(){
-        for(int i = 0 ; i < 3 ; i++){
-            if(board[i][0]== player && board[i][1]==player && board[i][2]==player) return true;
-            if(board[0][i]== player && board[1][i]==player && board[2][i]==player) return true;
-            if(board[0][0]== player && board[1][1]==player && board[2][2]==player) return true;
-            if(board[0][2]== player && board[1][1]==player && board[2][0]==player) return true;
-        }
+    bool win() {
+        for(int i=0;i<3;i++)
+            if(board[i][0]==player && board[i][1]==player && board[i][2]==player)
+                return true;
+
+        for(int i=0;i<3;i++)
+            if(board[0][i]==player && board[1][i]==player && board[2][i]==player)
+                return true;
+
+        if(board[0][0]==player && board[1][1]==player && board[2][2]==player) return true;
+        if(board[0][2]==player && board[1][1]==player && board[2][0]==player) return true;
 
         return false;
     }
 
-    void play() override{
-        int moves = 0 ; 
-        while (moves<9){
+    // ---- Polymorphic Function ----
+    void play() override {
+        int moves = 0;
+
+        while(moves < 9) {
             display();
             int r, c;
+            cout << "\nPlayer " << player << " enter row & column (0-2): ";
+            cin >> r >> c;
 
-            cout<<"Enter row and column ";
-            cin>>r;
-            cin>>c;
-            cout<<endl;
-
-            if(r<0 || r>2 || c<0 || c>2){
-                cout<<"invalid coordinates"<<endl;
+            if(r<0 || r>2 || c<0 || c>2 || board[r][c] != ' ') {
+                cout << "Invalid move! Try again.\n";
                 continue;
             }
-            board[r][c]=player;
+
+            board[r][c] = player;
             moves++;
 
-            if(win()){
-                cout<<"player "<<player<<" won the game"<<endl;
-                return ;
+            if(win()) {
+                display();
+                cout << "\nPlayer " << player << " wins!\n";
+                return;
             }
 
-            if (player == 'X') {
-    player = 'O';
-} else {
-    player = 'X';
-}
-
+            player = (player == 'X') ? 'O' : 'X';
         }
+
         display();
-
-
+        cout << "\nIt's a Draw!\n";
     }
 };
 
-int main(){
-    Game* g;
-    Tic t;
-    g=&t;
-    g->play();
+int main() {
+    TicTacToe t;
+t.play();
+       // polymorphic call
+
     return 0;
 }
